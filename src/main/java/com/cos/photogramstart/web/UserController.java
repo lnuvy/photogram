46f085fd34154151fe7e15db8c5957e91d421128
lnuvy <hangeulgod@naver.com>
2021-11-12 11:46:19 +0900

    30강 적용

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +17,16 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+		// 1. 정석적인 방법
+		System.out.println("세션 정보 : " + principalDetails.getUser());
+
+		// 2. 극혐
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		PrincipalDetails pd = (PrincipalDetails)auth.getPrincipal();
+		System.out.println("직접 찾은 정보 : " + pd.getUser());
+
 		return "user/update";
 	}
 	
